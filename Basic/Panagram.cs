dee6f2fdf99837ded231fafb779296c7d67baf6f
Marco Tello <muchahuev@hotmail.com>
2018-09-14 16:38:36 -0500

    adding the panagram alphabet initialization

--- Basic/Panagram.cs
+++ Basic/Panagram.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 namespace Basic
 {
     public class Panagram
     {
         public string IsPanagram(string word)
+        {
+            List<KeyValuePair<string, bool>> alphabet = InitializeAlphabet();
+
+            return "NO";
+        }
+
+        public List<KeyValuePair<string, bool>> InitializeAlphabet()
         {
             throw new NotImplementedException();
         }
