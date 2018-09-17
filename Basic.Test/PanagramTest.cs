97d4fe453a52bec61f1e32955eb77d0beea886c5
Marco Tello <muchahuev@hotmail.com>
2018-09-17 15:22:06 -0500

    finishig the panagram practice

--- Basic.Test/PanagramTest.cs
+++ Basic.Test/PanagramTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Basic.Test
@@ -22,6 +23,49 @@ namespace Basic.Test
         {
             string result = _panagram.IsPanagram(word);
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(ComparePanagramTestData))]
+        public void ComparePanagramTest(KeyValuePair<string, bool> expected, int expecterPosition, string word)
+        {
+            List<KeyValuePair<string, bool>> alphabet = _panagram.InitializeAlphabet();
+            alphabet = _panagram.ComparePanagram(word, alphabet);
+            Assert.Equal(expected, alphabet[expecterPosition]);
+        }
+
+        public static IEnumerable<object[]> ComparePanagramTestData()
+        {
+
+            return new List<object[]>
+                {
+                    new object[] { new KeyValuePair<string, bool>("A", true), 0, "abcdefghijklwnopqrstuvwxyz" },
+                    new object[] { new KeyValuePair<string, bool>("C", true), 2, "abcdefghijklwnopqrstuvwxyz" },
+                    new object[] { new KeyValuePair<string, bool>("Z", true), 25, "abcdefghijklwnopqrstuvwxyz" },
+                    new object[] { new KeyValuePair<string, bool>("M", false), 12, "abcdefghijklwnopqrstuvwxyz" },
+                    new object[] { new KeyValuePair<string, bool>("U", true), 20, "abcdefghijklwnopqrstuvwxyz" }
+                };
+        }
+
+        [Theory]
+        [MemberData(nameof(InitializeAlphabetTest))]
+        public void InitializeAphabetTest(KeyValuePair<string, bool> expected, int expecterPosition)
+        {
+            List<KeyValuePair<string, bool>> result = _panagram.InitializeAlphabet();
+            Assert.Equal(expected, result[expecterPosition]);
+        }
+
+        public static IEnumerable<object[]> InitializeAlphabetTest()
+        {
+
+            return new List<object[]>
+                {
+                    new object[] { new KeyValuePair<string, bool>("A", false), 0 },
+                    new object[] { new KeyValuePair<string, bool>("C", false), 2 },
+                    new object[] { new KeyValuePair<string, bool>("Z", false), 25 },
+                    new object[] { new KeyValuePair<string, bool>("M", false), 12 },
+                    new object[] { new KeyValuePair<string, bool>("U", false), 20 },
+                };
         }
     }
 }
--- Basic/Panagram.cs
+++ Basic/Panagram.cs
@@ -7,14 +7,59 @@ namespace Basic
     {
         public string IsPanagram(string word)
         {
+            string result = "SI";
+
             List<KeyValuePair<string, bool>> alphabet = InitializeAlphabet();
 
-            return "NO";
+            alphabet = ComparePanagram(word, alphabet);
+
+            foreach(KeyValuePair<string, bool> character in alphabet)
+            {
+                if(character.Value == false)
+                {
+                    return "NO";
+                }
+            }
+
+            return result; 
+        }
+
+        public string GetResult(List<KeyValuePair<string, bool>> alphabet)
+        {
+            throw new NotImplementedException();
         }
 
         public List<KeyValuePair<string, bool>> InitializeAlphabet()
         {
-            throw new NotImplementedException();
+            int aUnicode = 65;
+            char character;
+            List<KeyValuePair<string, bool>> alphabet = new List<KeyValuePair<string, bool>>();
+
+            for(int i = aUnicode; i < (aUnicode + 26); i++)
+            {
+                character = (char) i;
+                alphabet.Add( new KeyValuePair<string, bool>(character.ToString(), false) );
+            }
+
+            return alphabet;
+        }
+
+        public List<KeyValuePair<string, bool>> ComparePanagram(string word, List<KeyValuePair<string, bool>> alphabet)
+        {
+            char[] wordArray = word.ToUpper().ToCharArray();
+            
+            for(int i = 0; i < wordArray.Length; i++ )
+            {
+                for(int a = 0; a < alphabet.Count; a++)
+                {
+                    if(alphabet[a].Key.Equals(wordArray[i].ToString()))
+                    {
+                        alphabet[a] = new KeyValuePair<string, bool>(alphabet[a].Key, true);
+                    }
+                }
+            }
+
+            return alphabet;
         }
     }
 }
