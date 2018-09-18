2e9417fc39a41fadd111afef312136cb4c316f45
Marco Tello <muchahuev@hotmail.com>
2018-09-17 23:15:08 -0500

    finishing the baby food practice

--- Basic.Test/BabyFoodTest.cs
+++ Basic.Test/BabyFoodTest.cs
@@ -28,15 +28,82 @@ namespace Basic.Test
                     new object[] { new string[][] 
                         {  
                             new string[] { "SI", "patata", "maiz", "tomate", "FIN" },
-                            new string[] { "NO", "patata", "puerro", "guisantes", "pollo", "FIN" },
-                            new string[] { "SI", "tomate", "zanahoria", "puerro", "pollo", "calabacin", "arroz", "FIN" }
+                            new string[] { "NO", "patata", "puerco", "guisantes", "pollo", "FIN" },
+                            new string[] { "SI", "tomate", "zanahoria", "puerco", "pollo", "calabacin", "arroz", "FIN" }
                         },  new string[] { "guisantes" }
                     },
                     new object[] { new string[][] 
                         {  
                             new string[] { "SI", "tomate", "zanahoria", "pollo", "calabacin", "arroz", "FIN" },
-                            new string[] { "NO", "tomate", "ternera", "puerro", "FIN" }
-                        },  new string[] { "puerro", "ternera" }
+                            new string[] { "NO", "tomate", "ternera", "puerco", "FIN" }
+                        },  new string[] { "puerco", "ternera" }
+                    },
+                };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetBadFoodTestData))]
+        public void GetBadFoodTest(string[][] food, List<KeyValuePair<string, bool>> unlikedFood, string[] expected)
+        {
+            string[] result = _babyFood.GetBadFood(food, unlikedFood);
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetBadFoodTestData()
+        {
+
+            return new List<object[]>
+                {
+                    new object[] { new string[][] 
+                        {  
+                            new string[] { "SI", "patata", "maiz", "tomate", "FIN" },
+                            new string[] { "NO", "patata", "puerco", "guisantes", "pollo", "FIN" },
+                            new string[] { "SI", "tomate", "zanahoria", "puerco", "pollo", "calabacin", "arroz", "FIN" }
+                        },  
+                        new List<KeyValuePair<string, bool>> { new KeyValuePair<string, bool>("NO", false), new KeyValuePair<string, bool>("patata", false),
+                            new KeyValuePair<string, bool>("puerco", false), new KeyValuePair<string, bool>("guisantes", false), 
+                            new KeyValuePair<string, bool>("pollo", false), new KeyValuePair<string, bool>("FIN", false) },
+                        new string[] { "guisantes" }
+
+                    },
+                    new object[] { new string[][] 
+                        {  
+                            new string[] { "SI", "tomate", "zanahoria", "pollo", "calabacin", "arroz", "FIN" },
+                            new string[] { "NO", "tomate", "ternera", "puerco", "FIN" }
+                        },  
+                        new List<KeyValuePair<string, bool>> { new KeyValuePair<string, bool>("NO", false), new KeyValuePair<string, bool>("tomate", false), 
+                            new KeyValuePair<string, bool>("ternera", false), new KeyValuePair<string, bool>("puerco", false), 
+                            new KeyValuePair<string, bool>("FIN", false) },
+                        new string[] { "puerco", "ternera" }
+                    },
+                };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUnlikedFoodTestData))]
+        public void GetUnlikedFoodTest(string[][] food, KeyValuePair<string, bool> expected)
+        {
+            List<KeyValuePair<string, bool>> result = _babyFood.GetUnlikedFood(food);
+            Assert.Equal(expected, result[0]);
+        }
+
+        public static IEnumerable<object[]> GetUnlikedFoodTestData()
+        {
+
+            return new List<object[]>
+                {
+                    new object[] { new string[][] 
+                        {  
+                            new string[] { "SI", "patata", "maiz", "tomate", "FIN" },
+                            new string[] { "NO", "patata", "puerco", "guisantes", "pollo", "FIN" },
+                            new string[] { "SI", "tomate", "zanahoria", "puerco", "pollo", "calabacin", "arroz", "FIN" }
+                        },  new KeyValuePair<string, bool>("NO", false)
+                    },
+                    new object[] { new string[][] 
+                        {  
+                            new string[] { "SI", "tomate", "zanahoria", "pollo", "calabacin", "arroz", "FIN" },
+                            new string[] { "NO", "tomate", "ternera", "puerco", "FIN" }
+                        },  new KeyValuePair<string, bool>("NO", false)
                     },
                 };
         }
--- Basic/BabyFood.cs
+++ Basic/BabyFood.cs
@@ -1,10 +1,77 @@
+using System;
+using System.Collections.Generic;
+
 namespace Basic
 {
     public class BabyFood
     {
         public string[] GetFavoriteBabyFood(string[][] food)
         {
-            return null;
+            List<KeyValuePair<string, bool>> unlikeFood = GetUnlikedFood(food);
+            return GetBadFood(food, unlikeFood);
+        }
+
+        public string[] GetBadFood(string[][] food, List<KeyValuePair<string, bool>> unlikeFood)
+        {
+            List<string> badFood = new List<string>();
+            int counter = 0;
+
+            for(int lap = 0; lap < food.Length; lap++)
+            {
+                if(food[lap][0].Equals("SI"))
+                {
+                    while(counter < unlikeFood.Count)
+                    {
+                        for(int ingridient = 0; ingridient < food[lap].Length; ingridient++)
+                        {
+                            if(unlikeFood[counter].Key.Equals(food[lap][ingridient]))
+                            {
+                                unlikeFood[counter] = new KeyValuePair<string, bool>(unlikeFood[counter].Key, true);
+                            }
+                        }
+
+                        counter++;      
+                    }
+                }
+                counter = 0;
+            }
+
+            foreach(KeyValuePair<string, bool> ing in unlikeFood)
+            {
+                if(!ing.Key.Equals("NO"))
+                {
+                    if(!ing.Value)
+                    {
+                        badFood.Add(ing.Key);
+                    }
+                }
+            }
+            badFood.Sort();
+            return badFood.ToArray();
+        }
+
+        public List<KeyValuePair<string, bool>> GetUnlikedFood(string[][] food)
+        {
+            List<KeyValuePair<string, bool>> unlikedFood = new List<KeyValuePair<string, bool>>();
+            string[] badTasteFood = new string[0];
+
+            for(int lap = 0; lap < food.Length; lap++)
+            {
+                foreach(string ingridient in food[lap])
+                {
+                    if(ingridient.Equals("NO"))
+                    {
+                        badTasteFood = food[lap];
+                    }
+                }
+            }
+
+            foreach(string ingridient in badTasteFood)
+            {
+                unlikedFood.Add(new KeyValuePair<string, bool>(ingridient, false));
+            }
+
+            return unlikedFood;
         }
     }
 }
