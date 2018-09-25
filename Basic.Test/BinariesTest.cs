70483b9a42cbcf07c00121d46511f12ab24c4807
Marco Tello <muchahuev@hotmail.com>
2018-09-24 23:23:37 -0500

    finishing the binaries test

--- Basic.Test/BinariesTest.cs
+++ Basic.Test/BinariesTest.cs
@@ -10,6 +10,44 @@ namespace Basic.Test
             _binaries = new Binaries();
         }
 
-        //public void 
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(6, 4)]
+        [InlineData(2135, 14)]
+        public void GetMaximunTest(int originalNumber, int expected)
+        {
+            int result = _binaries.GetMaximun(originalNumber);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, new int[] {1})]
+        [InlineData(6, new int[] {2, 3, 2, 4})]
+        [InlineData(20, new int[] {2, 4, 3, 4, 2, 6, 5, 6, 3, 5, 4})]
+        public void GetConvinationsTest(int original, int[] expected)
+        {
+            int[] result = _binaries.GetConvinations(original);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] {1, 3, 2}, 3)]
+        [InlineData(new int[] {2, 3, 2, 4}, 4)]
+        [InlineData(new int[] {2, 4, 3, 4, 2, 6, 5, 6, 3, 5, 4}, 6)]
+        public void GetMaximunValueTest(int[] convinations, int expected)
+        {
+            int result = _binaries.GetMaximunValue(convinations);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, 3, 3)]
+        [InlineData(15, 5, 6)]
+        [InlineData(6, 0, 2)]
+        public void GetTotalOfOnesTest(int firstNumber, int secondNumber, int expected)
+        {
+            int result = _binaries.GetTotalOfOnes(firstNumber, secondNumber);
+            Assert.Equal(expected, result);
+        }
     }
 }
--- Basic/Binaries.cs
+++ Basic/Binaries.cs
@@ -1,7 +1,57 @@
+using System;
+using System.Collections.Generic;
+
 namespace Basic
 {
     public class Binaries
     {
-        
+        public int GetMaximun(int originalNumber)
+        {
+            int[] convinations = GetConvinations(originalNumber);
+            return GetMaximunValue(convinations);
+        }
+
+        public int GetMaximunValue(int[] convinations)
+        {
+            Array.Sort(convinations);
+            return convinations[convinations.Length-1];
+        }
+
+        public int[] GetConvinations(int originalNumber)
+        {
+            if(originalNumber == 1)
+            { 
+                return new int[]{ 1 };
+            }
+
+            double half = originalNumber / 2;
+            List<int> values = new List<int>();
+            int secondNumber = 0;
+
+            for(int i = originalNumber; i >= Math.Truncate(half); i--)
+            {
+                secondNumber = originalNumber - i;
+                values.Add(GetTotalOfOnes(i, secondNumber));
+            }
+
+            return values.ToArray();
+        }
+
+        public int GetTotalOfOnes(int firstNumber, int secondNumber)
+        {
+            string binary = Convert.ToString(firstNumber, 2);
+            binary = binary + Convert.ToString(secondNumber, 2);
+            char[] chars = binary.ToCharArray();
+            int result = 0;
+
+            foreach(int character in chars)
+            {
+                if(character == '1')
+                {
+                    result+=1;
+                }
+            }
+            return result;
+        }
     }
 }
